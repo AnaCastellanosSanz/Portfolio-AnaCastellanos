ab2246802f662dbeac9870cd1b1923b4b55ee415
AnaCastellanosSanz <113558604+AnaCastellanosSanz@users.noreply.github.com>
2023-11-07 17:28:45 +0100

    Actualización CV

--- src/Components/pages/Cv.jsx
+++ src/Components/pages/Cv.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./CV.scss";
-import cv from "../assets/cvPhoto.jpg"
+import cv from "../assets/cvPhoto.png"
 import pdf from "../CV/CV.pdf"
 import download from "../CV/download.png"
 
